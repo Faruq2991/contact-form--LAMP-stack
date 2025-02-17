7692f6660c4b7c56378a2d21a16724bcd9bbc500
Faruq2991 <162126839+Faruq2991@users.noreply.github.com>
2025-02-17 13:09:01 +0000

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'faruq');
+define('DB_PASS', 'mandzukic');
+define('DB_NAME', 'contact_form_db');
+define('ADMIN_EMAIL', 'faruqadam.92@gmail.com');
+?>
